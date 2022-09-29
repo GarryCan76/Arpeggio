ff9792a18bc0536f84e28e031dce704d09581dfc
Garon <garryenderson76@gmail.com>
2022-09-29 20:46:45 +0200

    ok

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-var context = new AudioContext()
-var sound = context.createOscillator()
-var volume = new GainNode(context,{gain:0.1})
-var frequency = 1
-var pitch = true
-let frequency_change_val = 100
-let time_out = 100
+let context = new AudioContext();
+let sound = context.createOscillator();
+let volume = new GainNode(context,{gain:0.1});
+let frequency = 1;
+let pitch = true;
+let frequency_change_val = 100;
+let time_out = 100;
 let maininterval;
-let range_witdh = 500
-let pitch_point = 300
+let range_witdh = 500;
+let pitch_point = 300;
+let connected = false;
+const visual_fs = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20];
+
 sound.start()
-var connected = false
 volume.connect(context.destination)
 function play() {
     if (connected === false){
@@ -46,7 +48,6 @@ pitcher.oninput = function () {
 }
 maininterval = setInterval(picher,time_out)
 function picher() {
-    console.log(time_out)
     if (pitch === true){
         frequency = frequency + frequency_change_val;
         if (frequency > pitch_point + range_witdh){
@@ -59,5 +60,22 @@ function picher() {
             pitch = true;
         }
         sound.frequency.value = frequency;
+    }
+    visual_fs.shift()
+    visual_fs.push(frequency / 10)
+    console.log(visual_fs)
+    if (connected === true) {
+        document.getElementById("vnode_1").style.height = "" + visual_fs[0] + "px";
+        document.getElementById("vnode_2").style.height = "" + visual_fs[1] + "px";
+        document.getElementById("vnode_3").style.height = "" + visual_fs[2] + "px";
+        document.getElementById("vnode_4").style.height = "" + visual_fs[3] + "px";
+        document.getElementById("vnode_5").style.height = "" + visual_fs[4] + "px";
+        document.getElementById("vnode_6").style.height = "" + visual_fs[5] + "px";
+        document.getElementById("vnode_7").style.height = "" + visual_fs[6] + "px";
+        document.getElementById("vnode_8").style.height = "" + visual_fs[7] + "px";
+        document.getElementById("vnode_9").style.height = "" + visual_fs[8] + "px";
+        document.getElementById("vnode_10").style.height = "" + visual_fs[9] + "px";
+        document.getElementById("vnode_11").style.height = "" + visual_fs[10] + "px";
+        document.getElementById("vnode_12").style.height = "" + visual_fs[11] + "px";
     }
 }
